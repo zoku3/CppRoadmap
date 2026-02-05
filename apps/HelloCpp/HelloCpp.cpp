48bc5b7c551180d64e10511aed163018df5efea0
kiga112 <purezy523@gmail.com>
2026-02-06 01:42:28 +0900

    Implement basic I/O and Clamp

--- apps/HelloCpp/HelloCpp.cpp
+++ apps/HelloCpp/HelloCpp.cpp
@@ -2,9 +2,43 @@
 //
 
 #include <iostream>
+#include <string>
+
+	//xをminval～maxvalに収めて返す
+int Clamp(int x, int minval, int maxval)
+{
+	if (x < minval) return minval;
+	if (x > maxval) return maxval;
+	return x;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    return 0;
+	//名前入力
+	std::string name;
+	std::cout << "名前を入力: ";
+	std::getline(std::cin, name);
+	std::cout << "こんにちは, " << name << "!\n";
+
+	//足し算
+	int a = 0, b = 0;
+	std::cout << "整数aを入力: ";
+	std::cin >> a;
+	std::cout << "整数a :" << a << "\n";
+	std::cout << "整数bを入力";
+	std::cin >> b;
+	std::cout << "整数b ;" << b << "\n";
+	std::cout << "a + b = " << (a + b) << "\n";
+	std::cout << "a - b = " << (a - b) << "\n";
+	std::cout << "a * b = " << (a * b) << "\n";
+	std::cout << "a / b = " << (a / b) << "\n";
+	std::cout << "a % b = " << (a % b) << "\n";
+	//c:clamp
+	int x = 0;
+	std::cout << "Xを入力（０～１００に収める）: ";
+	std::cin >> x;
+	std::cout << "Clamp結果　＝　" << Clamp(x, 0, 100) << "\n";
+
+	return 0;
+	
 }
